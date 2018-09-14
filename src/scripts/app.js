61b4584539a247e24222d985b311350a4b0db430
Shai <shaistakirmani@gmail.com>
2018-09-14 15:18:28 -0400

    added scripts

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,17 @@
-const hello = 'World';
-console.log(hello);
+$(function () {
+    $("a").smoothScroll({ speed: 2000 });
+
+    $('ham-menu').click(function(){
+        ('head-nav').show('slow');
+    });
+ 
+});
+
+const typed = new Typed('.header-p', {
+    strings: ['Front End Web Developer'],
+    typeSpeed: 60,
+    loop: true,
+    loopCount: 4,
+    showCursor:false,
+
+});
